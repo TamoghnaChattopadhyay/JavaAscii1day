4d979dc83879ab97aada263fae96c7e9feed1785
TamoghnaChattopadhyay <tchitamo3@gmail.com>
2017-01-07 23:48:58 +0530

    JAVAASCII

---
+++ JavaASCII.java
@@ -0,0 +1,16 @@
+public class JavaASCII {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		
+		for (int i = 65; i <= 90; i++) {
+			
+		System.out.println(i+"-"+(char) i);
+		}
+		
+		
+	}
+
+}
